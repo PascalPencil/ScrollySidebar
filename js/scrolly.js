0a34ed318a57e9124ffb8c25717bf43e9bee8164
Pascal Verstegen <pascal@superpencil.com>
2014-05-29 13:43:47 +0400

    If div touches top of container div do *

--- js/scrolly.js
+++ js/scrolly.js
@@ -1,1 +1,14 @@
-
+function isColliding(divName) {   
+  var posTop = $('#sidebar').scrollTop() - $(divName).offset().top - 1;
+  if (posTop >= 0) {
+    $(divName).css({
+      "background-color": "yellow",
+      "font-weight": "bolder"
+    });
+  }
+}
+$(document).ready(function() {
+  $('.scroll-pane').scroll(function(){
+    isColliding('.two');
+  }).trigger('scroll');
+});
