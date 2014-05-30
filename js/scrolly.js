6c70ebc50451abe2a36f1702eacda71494d5897f
Pascal Verstegen <pascal@superpencil.com>
2014-05-31 02:55:17 +0400

    Cleanup 1

--- js/scrolly.js
+++ js/scrolly.js
@@ -1,14 +1,4 @@
-$(document).ready(function() {
-
-  //Bind scroll event so still scrollable even if under fixed divs
-  $('.scroll-pane').bind('mousewheel', function(event) {
-    event.preventDefault();
-    var scrollTop = this.scrollTop;
-    this.scrollTop = (scrollTop + ((event.deltaY * event.deltaFactor) * -1));
-  });
-
-  //Calculate .section header height
-  var headerHeight = $('#sidebar .section h4').outerHeight();
+Scrolly = function(){
 
   //Load as function to support DOM injection
   function Scroller() {
@@ -29,82 +19,109 @@ $(document).ready(function() {
 
     //Each section do, integer starts at 0 to calculate stacking of headers on top
     $('.section').each(function(i) {
+
       //Calculate visible .scroll-pane height
       var parentHeight = $(this).parent().outerHeight() - elementCountBottom;
+
       //Start top position calculation at 0 and integer up for every section
       var posTop = $(this).position().top;
+
       //Start bottom position calculation at total height of all section and integer down for every section
       var posBottom = posTop + headerHeight;
+
       //If .section element touches top minus previous section headers, set position to absolute top
       if (posTop <= (i * headerHeight)) {
-        $('h4', this).addClass('snap').css({
+        $('.header', this).addClass('snap').css({
           'bottom': 'inherit',
           'top': (i * headerHeight + fixedTop) + 'px'
         });
+
         //Add padding to sibling .body element to account for the fixing of the header's position
-        $('h4', this).siblings().css('padding-top', headerHeight + 'px');
-        //If .section element touches bottom minus previous section headers, set position to absolute bottom
+        $('.header', this).siblings().css('padding-top', headerHeight);
+
+      //If .section element touches bottom minus previous section headers, set position to absolute bottom
       } else if (posBottom >= parentHeight) {
-        $('h4', this).addClass('snap').css({
+        $('.header', this).addClass('snap').css({
           'top': 'inherit',
           'bottom': elementCountBottom + 'px'
         });
-        //Else let section align in scroll naturally
+
+      //Else let section align in scroll naturally
       } else {
-        $('h4', this).removeClass('snap').css({
+        $('.header', this).removeClass('snap').css({
           'bottom': 'inherit',
           'top': 'inherit'
         });
+
         //Remove padding to sibling .body element to account for the fixing of the header's position
-        $('h4', this).siblings().css('padding-top', 'inherit');
+        $('.header', this).siblings().css('padding-top', 'inherit');
       }
+
       //Add -1 section header height
       elementCountBottom -= headerHeight;
     });
   }
 
+  //Bind scroll event so still scrollable even if under fixed divs
+  $('.scroll-pane').bind('mousewheel', function(event) {
+    event.preventDefault();
+    var scrollTop = this.scrollTop;
+    this.scrollTop = (scrollTop + ((event.deltaY * event.deltaFactor) * -1));
+  });
+
+  //Calculate .section header height
+  var headerHeight = $('#sidebar .section .header').outerHeight();
+
+  //If tick is clicked collapse section and update sidebar
+  $('.section').on( "click", ".header", function() {
+    $(this).closest('.section').children('.body').toggle();
+    $(this).closest('.section').toggleClass('collapsed');
+    Scroller();
+  });
+
   //ScrollTo
-  $('.snap').on( "click", ".link", function() {
-    $(this).siblings().show();
-    $(this).find('.tick').removeClass('ticked');
+  $('.section').on( "click", ".snap", function() {
+
+    //Uncollapse section
+    $(this).children('.body').show();
+    $(this).removeClass('.collapsed');
+
     //Current amount of scroll
     var scrolled = $('.scroll-pane').scrollTop();
+
     //Get section number
     var sectionNumber = $(this).closest('.section').index();
+
     //Linenumber to push properly from top
     var lineNumber = sectionNumber * headerHeight;
+
     //Animate to div
     var topPosition = scrolled + $(this).closest('.section').position().top - lineNumber;
     $('.scroll-pane').animate({
       scrollTop: topPosition
     });
+
     Scroller();
   });
-
-  //If tick is clicked collapse section and update sidebar
-  $('.section .tick').click(function() {
-    $(this).closest('.section').children('.body').toggle();
-    $(this).toggleClass('ticked');
-    Scroller();
-  });
-
-  //Call plugin
-  Scroller();
 
   //Call plugin on scroll in the .scroll-pane
-  $('.scroll-pane').scroll(function() {
-    Scroller();
-  }).trigger('scroll');
+  $('.scroll-pane').scroll(function() { Scroller(); }).trigger('scroll');
 
   //Call plugin on window resizing
-  window.onresize = function() {
-    Scroller();
-  };
+  window.onresize = function() { Scroller(); };
 
-  //DOM injecting Section Seven after 5 seconds for demo purposes
-  setTimeout(function() {
-    $('.scroll-pane').append("<div class=\"section seven\"><h4 class=\"title\"><a href=\"#\" class=\"label\"><div class=\"tick sprite-mid\"><i></i></div> Section 7<i class=\"status sprite-main\"></i></a></h4><div class=\"body\"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porta ipsum a leo malesuada, sed aliquet justo eleifend. Sed vehicula pretium libero, id congue purus lobortis eget. Maecenas iaculis congue vehicula. Aliquam erat volutpat. Sed vitae facilisis nisi, nec scelerisque nulla. Maecenas a lacus euismod, tincidunt mauris at, sagittis tortor. Donec ullamcorper tortor arcu, a malesuada diam posuere id. Phasellus id ullamcorper quam.<\/p><\/div><\/div>");
-    //Call the functions to reload the sidebar
-    Scroller();
-  }, 2000);
+  //Call function
+  Scroller();
+}
+
+$(document).ready(function() {
+  Scrolly();
 });
+
+//DOM injecting Section Seven after 5 seconds for demo purposes
+setTimeout(function() {
+  $('.scroll-pane').append("<div class=\"section\"><a href=\"#\" class=\"header\"><span class=\"tick\">&#9660;</span><span class=\"ticked\">&#9654;</span>  <span class=\"link\">Section 7</span></a><div class=\"body\"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porta ipsum a leo malesuada, sed aliquet justo eleifend. Sed vehicula pretium libero, id congue purus lobortis eget. Maecenas iaculis congue vehicula. Aliquam erat volutpat. Sed vitae facilisis nisi, nec scelerisque nulla. Maecenas a lacus euismod, tincidunt mauris at, sagittis tortor. Donec ullamcorper tortor arcu, a malesuada diam posuere id. Phasellus id ullamcorper quam.</p></div></div>");
+
+  //Call the plugin to reload the sidebar
+  Scrolly();
+}, 2000);
